8c2219a7b79c4023dec7e957d2b950f7599c742c
mohamedyahya <mohamed.yahya.dev@gmail.com>
2022-02-04 15:18:18 +0100

    exercice-add

---
+++ exercice/post.php
@@ -0,0 +1,10 @@
+<?php
+    if($_POST['FirstName'] || $_POST['email']  || $_POST['Write-message']){
+        
+        echo "FirstName " . " : " . $_POST['FirstName'] . "<br />";
+        echo "email  "  . " : ". $_POST['email'] . " <br />";
+        echo "message " . " : ". $_POST['Write-message'] . " <br />";
+
+    };
+
+?>
