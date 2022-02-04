8d9d5ea41fcabb0616b8db90891bc6b77909c9ff
mohamedyahya <mohamed.yahya.dev@gmail.com>
2022-02-04 10:38:34 +0100

    arrays-exemple-2 add

---
+++ Arrays/arrays-2.php
@@ -0,0 +1,13 @@
+<?php
+
+$Phones = array (
+  array("samsung",22,18),
+  array("iphone",15,13),
+  array("Nokia",5,2),
+);
+  
+echo $Phones[0][0].": In stock: ".$Phones[0][1].", sold: ".$Phones[0][2].".<br>";
+// echo $cars[1][0].": In stock: ".$cars[1][1].", sold: ".$cars[1][2].".<br>";
+// echo $cars[2][0].": In stock: ".$cars[2][1].", sold: ".$cars[2][2].".<br>";
+// echo $cars[3][0].": In stock: ".$cars[3][1].", sold: ".$cars[3][2].".<br>";
+?>
