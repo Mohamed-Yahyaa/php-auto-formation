550a90a0eaa0c8168f3b9d39260957a29560ecc1
mohamedyahya1403 <92155140+Mohamed-Yahyaa@users.noreply.github.com>
2022-02-13 16:29:52 +0100

    loop-for-add

--- exercice/post.php
+++ exercice/post.php
@@ -2,7 +2,7 @@
     if($_POST['FirstName'] || $_POST['email']  || $_POST['Write-message']){
         
         echo "FirstName " . " : " . $_POST['FirstName'] . "<br />";
-        echo "email  "  . " : ". $_POST['email'] . " <br />";
+        echo "email  " . " : ". $_POST['email'] . " <br />";
         echo "message " . " : ". $_POST['Write-message'] . " <br />";
 
     };
---
+++ loop/for.php
@@ -0,0 +1,11 @@
+<?php  
+for ($x = 0; $x <= 4; $x++) {
+  echo "The number is: $x <br>";
+}
+
+// this number is 0
+// this number is 1
+// this number is 2
+// this number is 3
+// this number is 4
+?>  
