7018371a1c93a6c9d9d5365a7100c28acf4d6dd8
mohamedyahya <mohamed.yahya.dev@gmail.com>
2022-02-15 11:01:21 +0100

    require-add

--- file-inclusion/ex2/index.php
+++ file-inclusion/ex2/index.php
@@ -7,8 +7,8 @@
     <title>Document</title>
 </head>
 <body>
-       <?php require("xxmenu.php"); ?>
-       <p>This is an example to show how to include wrong PHP file!</p> 
+      <?php require("xxmenu.php"); ?>
+       <p>This is an example to show how to include wrong PHP file!</p>
     
 </body>
 </html>
