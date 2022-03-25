7a2a6b4f734be3be9d8b1ac6941ea70ac9ca843b
mohamedyahya <mohamed.yahya.dev@gmail.com>
2022-03-25 13:39:02 +0100

    Session - Add

---
+++ session/index.php
@@ -0,0 +1,15 @@
+<?php session_start();?>
+
+
+<html>
+    <body>
+        <form action="session.php" method='post'  >
+<input type="text" name='name'  placeholder="fname">
+<input type="text" name ='age' placeholder="age"> 
+<input type="submit" name="btn" value='login'>
+
+</form>
+
+
+</body>
+</html>
