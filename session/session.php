aaefd03ba82218f72ca95ac02453d273c14d33fd
mohamedyahya <mohamed.yahya.dev@gmail.com>
2022-03-25 13:44:42 +0100

    session - update

---
+++ session/session.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = $_POST["name"];
+$age = $_POST["age"];
+
+
+if(isset($_POST['btn'])){
+    echo  " your Name is  : ".$_SESSION['Name'] = $name ; 
+    echo  "<br>"."your age is : ". $_SESSION["Age"]=$age;
+    
+}
+
+
+?>
